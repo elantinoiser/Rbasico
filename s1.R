1e7ad1905e71eac9aba1e78504dbf6c1c715f418
elantinoiser <69495824+elantinoiser@users.noreply.github.com>
2021-03-23 11:55:31 -0600

    Update s1.R

--- s1.R
+++ s1.R
@@ -61,3 +61,91 @@ list.files()
 ls()
 
 ?ls
+
+rm(list=ls()) #Eliminar todos los objetos del espacio de trabajo
+
+?list.files
+args(list.files)
+
+list.files(path = ".")
+
+list.files(path = "C:/Users/rdelatorre/")
+
+list.files(path = "C:/")
+
+list.files(path = ".")
+
+#Cambiar nombre de archivos
+
+file.rename("mytest.R", "mytest2.R")
+
+path<- file.path("C:", "Users", "rdelatorre", 
+"Documents", "GitHub", "Rbasico", fsep = "/")
+
+setwd(path)
+
+setwd("C:/Users/rdelatorre/Documents/GitHub/")
+
+getwd()
+?setwd
+
+getwd()
+
+dir.create("materias/filosofia/lecturas")
+
+dir.create(file.path("biologia", "lecturas"),
+           recursive = TRUE)
+
+file.create("jc.txt")
+file.create("prueba2.R")
+
+materias <- c("historia", "geografia", "fisica", "quimica")
+
+for (i in materias){
+  dir.create(i)
+  
+}
+
+for (i in materias){
+  
+  dir.create(file.path(i, "lecturas"), recursive = TRUE)
+}
+
+seq(1,20)
+seq(20,1)
+
+seq(1,20, by=0.5)
+
+seq(1,20, length=10)
+
+my_seq <- seq(5,10, length=30)
+
+1:length(my_seq) #Una secuencia de la misma longitud que otra
+
+seq_along(my_seq)
+
+#Crear un vector que contenga 40 ceros
+
+rep(0, times=40)
+
+rep(c(0,1,2), each=10)
+
+for(i in 1:4){
+  print(seq(1,5))
+  
+}
+
+vect <- c(0.5, 55, -10, 6)
+tf <- (vect<1)
+
+vect>=6
+
+3>5
+
+4==4
+
+(3>5) & (4==4)
+
+(3>5) | (4==4)
+
+#push
